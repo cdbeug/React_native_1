309af1fecf21aaf2729920a7dbf21e9b27959b2e
Cyrille DE GASQUET <cyrille.degasquet2@gmail.com>
2023-02-24 11:58:19 +0100

    Ajout de l'accès à la caméra

--- src/component/screens/Cam/Cam.jsx
+++ src/component/screens/Cam/Cam.jsx
@@ -1,16 +1,42 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { Camera } from 'expo-camera';
+import { useEffect } from 'react';
 
 const Cam = () => {
+	const [permission, requestPermission] = Camera.useCameraPermissions();
+	let size = Dimensions.get("window");
+	const largeurCamera = size.width;
+	const hauteurCamera = (size.width * 16) / 9;
+
+	useEffect(() => {
+		requestPermission();
+	}, []
+	);
+
+
+	console.log(permission);
+	if (!permission) {
+		return <Text>Veuillez  autoriser la camera</Text>
+	}
+
+	if (!permission.granted) {
+		return <Text>Vous avez refusé l'utilisation de la caméra</Text>
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text>Cam</Text>
-		</View>
+			<Camera ratio="16.9"
+				style={{ width: largeurCamera, height: hauteurCamera }}
+			></Camera>
+		</View >
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		width: "100%",
+		height: "100%"
+	},
 });
 
-//make this component available to the app
 export default Cam;
